b56b2a3b78daa268fd2909e49a021a94ae3c65c1
ZachBlaustone <Zachblaustone@gmail.com>
2016-07-20 14:01:05 -0500

    Initial Commit

---
+++ recipeZ/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  recipeZ
+//
+//  Created by Zachary Blaustone on 7/20/16.
+//  Copyright © 2016 Pryzm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
